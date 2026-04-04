5661677895812f6441630f9fe9761354cd4abea6
Ludmila (DeFinn) <ludmila.omlopes@gmail.com>
2026-04-04 19:54:20 -0300

    feat: initial commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,42 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function formatPipetz(value: number) {
-  return new Intl.NumberFormat("pt-BR").format(value);
-}
-
-/** @deprecated Use formatPipetz */
-export const formatPoints = formatPipetz;
-
-export function formatDateTime(value: Date | string | null | undefined) {
-  if (!value) {
-    return "agora";
-  }
-
-  const date = typeof value === "string" ? new Date(value) : value;
-  return new Intl.DateTimeFormat("pt-BR", {
-    dateStyle: "short",
-    timeStyle: "short",
-  }).format(date);
-}
-
-export function slugify(input: string) {
-  return input
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/(^-|-$)/g, "");
-}
-
-export function shortCode(size = 6) {
-  const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
-  return Array.from({ length: size }, () => {
-    const index = Math.floor(Math.random() * alphabet.length);
-    return alphabet[index];
-  }).join("");
+  return twMerge(clsx(inputs))
 }
